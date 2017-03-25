fa1382f700ed2ca4890a855a23c4ec1237b5269a
Vu Duy Du <vuduydu1997@gmail.com>
2017-03-25 13:43:50 +0530

    Second Commit

--- PatternRecognization/PatternRecognization/Main.cpp
+++ PatternRecognization/PatternRecognization/Main.cpp
@@ -1,15 +1,27 @@
 #include "Main.h"
 
-int main() {
+int main(int argc, char**argv) {
 	Main mainProject();
 	return 0;
 }
 
 Main::Main()
+	:window("Pattern Recognization",)
 {
+	//std::cout << "Hello Neural Network.\n";
+	setup();
+	loop();
 }
 
 
 Main::~Main()
+{
+}
+
+void Main::setup()
+{
+}
+
+void Main::loop()
 {
 }
--- PatternRecognization/PatternRecognization/Main.h
+++ PatternRecognization/PatternRecognization/Main.h
@@ -1,9 +1,17 @@
 #pragma once
-
+#include"GlobalHeader.h"
+#include"Window.h"
+#include"Clock.h"
 class Main
 {
 public:
 	Main();
 	~Main();
+private:
+	void setup();
+	void loop();
+	Window window;
+	SDL_Renderer*renderer;
+	Clock clock;
 };
 
